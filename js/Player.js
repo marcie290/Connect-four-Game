b2d2f714ce6d1793498a9c08aa2244c318ee36d0
2522237y <2522237y@gmail.com>
2021-03-26 10:06:06 +0100

    init

---
+++ js/Player.js
@@ -0,0 +1,9 @@
+class Player {
+    constructot(name, id, color, active = false){
+        this.name = name;
+        this.id = id;
+        this.color = color;
+        this.active = active;
+        this.tokens = [];
+    }
+}
